42ca8505582a953dd7d0609654cc53c1e739efec
Yan Lima Barbosa <yanbr763@gmail.com>
2024-11-07 23:52:50 -0300

    feat: add text component and storybook
    Adds a new storybook for the Text component, showcasing its basic usage and customization options with tags. This will improve documentation and provide a better understanding of the component's functionality.

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos quis, ratione eligendi vitae adipisci soluta aliquid ea cum tempore, repudiandae explicabo repellat minima deleniti, qui enim libero asperiores autem quisquam?',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
