de053abc4eb652ad7273cbc42ee63a0c8d5c6edf
Yan Lima Barbosa <yanbr763@gmail.com>
2025-02-15 02:20:53 -0300

    feat: add onClick action for button component

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -1,37 +1,43 @@
 import type { StoryObj, Meta } from '@storybook/react'
 import { Button, ButtonProps } from '@ignite-ui/react'
-import { ArrowRight } from "phosphor-react"
+import { ArrowRight } from 'phosphor-react'
 
 export default {
   title: 'Form/Button',
   component: Button,
   args: {
-    children: "Send"
+    children: 'Send',
+  },
+  argTypes: {
+    onClick: {
+      action: 'click',
+    },
   },
 } as Meta<ButtonProps>
 
 export const Primary: StoryObj<ButtonProps> = {}
 
 export const Secondary: StoryObj<ButtonProps> = {
-  args: { variant: "secondary", children: "Create new" }
+  args: { variant: 'secondary', children: 'Create new' },
 }
 export const Tertiary: StoryObj<ButtonProps> = {
-  args: { variant: "tertiary", children: "Cancel" }
+  args: { variant: 'tertiary', children: 'Cancel' },
 }
 
 export const Small: StoryObj<ButtonProps> = {
-  args: { size: "sm" }
+  args: { size: 'sm' },
 }
 
 export const WithIcon: StoryObj<ButtonProps> = {
   args: {
     children: (
-      <>Próximo Passo <ArrowRight weight='bold' /></>
-    )
-  }
+      <>
+        Próximo Passo <ArrowRight weight="bold" />
+      </>
+    ),
+  },
 }
 
 export const Disabled: StoryObj<ButtonProps> = {
-  args: { disabled: true }
+  args: { disabled: true },
 }
-
