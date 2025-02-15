5e72668d7a4b7f781df9d13fa645f6d7283bd749
Yan Lima Barbosa <yanbr763@gmail.com>
2025-02-15 02:43:00 -0300

    feat: add Text component size controls for storybook

--- packages/docs/src/stories/Text.stories.tsx
+++ packages/docs/src/stories/Text.stories.tsx
@@ -5,8 +5,31 @@ export default {
   title: 'Typography/Text',
   component: Text,
   args: {
+    size: 'md',
     children:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos quis, ratione eligendi vitae adipisci soluta aliquid ea cum tempore, repudiandae explicabo repellat minima deleniti, qui enim libero asperiores autem quisquam?',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio',
+      },
+    },
   },
 } as Meta<TextProps>
 
