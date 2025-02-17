b5274256942d713f0f755d76b5abd4a87b9d00ca
Yan Lima Barbosa <yanbr763@gmail.com>
2025-02-17 06:33:56 -0300

    feat: design system refinements

--- packages/react/src/components/Box.tsx
+++ packages/react/src/components/Box.tsx
@@ -1,13 +1,16 @@
+import { ElementType } from 'react'
 import { styled } from '../styles'
 import { ComponentProps } from '@stitches/react'
 
 export const Box = styled('div', {
-  padding: '$4',
+  padding: '$6',
   borderRadius: '$md',
   backgroundColor: '$gray800',
   border: '1px solid $gray600',
 })
 
-export interface BoxProps extends ComponentProps<typeof Box> {}
+export interface BoxProps extends ComponentProps<typeof Box> {
+  as?: ElementType
+}
 
 Box.displayName = 'Box'
